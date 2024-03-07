96435c8f5237ebe3fdebb266c75d2a3af168bf19
Jhony Burbano <jhonyburbanojb@gmail.com>
2024-03-07 15:57:38 -0500

    initial config project

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.APP_ENV = process.env.APP_ENV || "development";
+
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: `${__dirname}/../config/${process.env.APP_ENV}.env`,
+});
+
+console.log(process.env.APP_FOO);
+import express from "express";
+
+const app: express.Application = express();
+
+app.get("/", (req, res) => {
+  res.send("Running ...");
+});
+
+export { app };
